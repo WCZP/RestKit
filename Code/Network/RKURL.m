adc661539421741dbc0e001707683aade2b2a9ed
Michael Fleet <michael.r.fleet@jpmchase.com>
2012-06-12 11:02:10 -0400

    Removed inline patch (hack) to RKURL.

--- Code/Network/RKURL.m
+++ Code/Network/RKURL.m
@@ -71,17 +71,7 @@
     // Build the new URL path
     NSRange queryCharacterRange = [theResourcePath rangeOfCharacterFromSet:[NSCharacterSet characterSetWithCharactersInString:@"?"]];
     NSString *resourcePathWithoutQueryString = (queryCharacterRange.location == NSNotFound) ? theResourcePath : [theResourcePath substringToIndex:queryCharacterRange.location];
-
-    // JPMC HACK:
-    //EB: in some cases, while we are debugging using Mockey, the URL is not standard, and some characters are missing 
-    //    after stringByStandardizingPath method is sent, causing the url being changed, therefore services are not responding.
-    #ifdef DEBUG    
-        NSString *baseURLPath = [theBaseURL path];
-    #else
-        NSString *baseURLPath = [[theBaseURL path] isEqualToString:@"/"] ? @"" : [[theBaseURL path] stringByStandardizingPath];
-    #endif
-    // END JPMC HACK
-
+    NSString *baseURLPath = [[theBaseURL path] isEqualToString:@"/"] ? @"" : [[theBaseURL path] stringByStandardizingPath];
     NSString *completePath = resourcePathWithoutQueryString ? [baseURLPath stringByAppendingString:resourcePathWithoutQueryString] : baseURLPath;
     NSString* completePathWithQuery = [completePath stringByAppendingQueryParameters:mergedQueryParameters];
     
